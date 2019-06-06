ad0d51304c2a2bd5a3fd2fc1b1a5759e510cfb43
randomslap <kentokazaki@gmail.com>
2019-06-05 19:10:39 -0700

    replaced old files and added boilerplate files

--- schema.sql
+++
@@ -1,2 +0,0 @@
--- DROP DATABASE IF EXISTS db_name;
--- CREATE DATABASE db_name;
--- server.js
+++ server.js
@@ -1,17 +1,47 @@
+require("dotenv").config();
 var express = require("express");
+var exphbs = require("express-handlebars");
+
+var db = require("./models");
+
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
-var db = require("./models"); // requires models for syncing
-
-app.use(express.urlencoded({ extended: true })); // sets up the Express app to handle data parsing
+// Middleware
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static("public")); // static directory
+app.use(express.static("public"));
 
-require("./routes/api-routes.js")(app);
+// Handlebars
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
 
-db.sequelize.sync({ force: true }).then(function() { // syncs sequelize models and then starts Express app
+// Routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+var syncOptions = { force: false };
+
+// If running a test, set syncOptions.force to true
+// clearing the `testdb`
+if (process.env.NODE_ENV === "test") {
+  syncOptions.force = true;
+}
+
+// Starting the server, syncing our models ------------------------------------/
+db.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
   });
 });
+
+module.exports = app;
