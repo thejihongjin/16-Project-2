4acdf39c5dd62dc405d5c7133cf1a651f47e28d1
jax <jjackson17mv@gmail.com>
2019-06-10 17:02:50 -0700

    first draft of api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,51 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  // Get all authors
+  app.get("/api/post", function(req, res) {
+    db.Bookshelf.findAll({}).then(function(dbBookshelfs) {
+      res.json(dbBookshelfs);
     });
   });
 
+  // Get route for returning authors of a specific category
+  app.get("/api/posts/category/:category", function(req, res) {
+    db.Bookshelf.findAll({
+      where: {
+        category: req.params.category
+      }
+    })
+      .then(function(dbBookshelfs) {
+        res.json(dbBookshelfs);
+      });
+  });
+
+  // Get route for retrieving a single author
+  app.get("/api/posts/:id", function(req, res) {
+    db.Bookshelf.findOne({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(function(dbBookshelf) {
+        res.json(dbBookshelf);
+      });
+  });
+
+
+
+
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  app.post("/api/post", function(req, res) {
+    db.Bookshelf.create(req.body).then(function(dbBookshelf) {
+      res.json(dbBookshelf);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+  app.delete("/api/post/:id", function(req, res) {
+    db.Bookshelf.destroy({ where: { id: req.params.id } }).then(function(dbBookshelf) {
+      res.json(dbBookshelf);
     });
   });
 };
